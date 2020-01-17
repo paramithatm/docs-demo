30cb4f70c736c707ed4931fb87018a5224a9dffb
Mitha <paramithatm@gmail.com>
2020-01-17 07:53:52 +0700

    bad code sample

--- DocumentationDemo/ViewController.swift
+++ DocumentationDemo/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var step: Int = 0
+    
+    private func loginScreen() {
+        print("Ini login")
+        step = 2
     }
-
-
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        trigger()
+    }
+    
+    private func showScreen() {
+        if step == 1 {
+            loginScreen()
+        } else if step == 2 {
+            homeScreen()
+        } else if step == 3 {
+            thankScreen()
+        }
+    }
+    
+    private func homeScreen() {
+        print("🏡📺")
+        step = 3
+    }
+    
+    private func thankScreen() { print("Thank you") }
+    
+    private func getStep() -> Int {
+        if step != 0 {
+            return fetchStep()
+        }
+        return 0
+    }
+    
+    private func fetchStep() -> Int {
+        // request network
+        return 1
+    }
+    
+    private func trigger() {
+        step = getStep()
+        showScreen()
+    }
 }
 
